743836fdab1b28998e3c1ad3aff657742e0434d4
Habieeb <habeebumar24@gmail.com>
2022-04-07 16:55:08 +0100

    My commit on 4-puts.c task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,7 @@
  *
  * Return: 0 after printing the function
  *\
-[A[C[C[C[C[A[A[A[A[D[D[/[B[B[B[B[C[/
 int main(void)
-puts("\"programming is the building of multilingual puzzle"):
-[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[{
 puts("\"programming is the building of multilingual puzzle"):
 return (0):
 }
