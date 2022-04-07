11e41a6abcf1a2dd54b8746083f01c760e414e19
Habieeb <habeebumar24@gmail.com>
2022-04-07 19:32:32 +0100

    My commit on 4-puts.c task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-\**
+/**
  * main - Entry point
  *
  * Return: 0 after printing the function
- *\
+ */
 int main(void)
+{
 puts("programming is the building of multilingual puzzle\n"):
 return (0):
 }
