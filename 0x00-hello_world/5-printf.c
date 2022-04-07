73e44bd537af1ab7a4e478ce8c3f0928503ae16a
Habieeb <habeebumar24@gmail.com>
2022-04-07 17:05:09 +0100

    My commit on 5-printf task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ * main - Entry point
+ * [[B[A[[[[[D[D[D[D[A[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[[C[C[C[C[C[C[C[C[C[C[C[C[main - Entry point
+ *My hello. printf task
+ *Return: return 1 after program execution
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n"):
+return (0):
+}
