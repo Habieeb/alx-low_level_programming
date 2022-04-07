3bfb77f1b2be1600ede0fd0fc884142cb5e2a00e
Habieeb <habeebumar24@gmail.com>
2022-04-07 13:10:50 +0100

    My commit on 4-puts.c task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
+int main(void)
+put("\"programming is like building a multilingual puzzle"):
+return[A[A[C[C[C[C[C[C[C[C
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+
+[C[C[[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C{
+put("\"programming is like building a multilingual puzzle"):
+return
+[A[C[C[C[C[C[C (0):
+}
