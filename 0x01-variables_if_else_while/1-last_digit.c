fc9dbac7d7401db502f5a25922ebd43fc5e6411e
Habieeb <habeebumar24@gmail.com>
2022-04-08 16:46:29 +0100

    My commit on 1-last_digit.c task

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always o (success)
+*/
+int main(void)
+
+{
+int n:
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n % 10 > 5)
+{
+printf("Last gigit of %i is %i and is greater than [A[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[A[C[C[C[C[C[C[D[:[B[B[B[C[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[A[A[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[A[A[D[D[D[D[D[A[;[A[A[D[D[D[D[D[D[D[D[;[B[B[B[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[d[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C 5\n", n, n % 10);
+}
+
+else if (n % 10 == 0)
+{printf("Last digit of %i is %i and is 0\n", n, n % 10);
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[
+printf("Last digit of %i is %i and is 0\n", n, n % 10);
+
+else
+{
+printf("Last digit of %i is %i and is less then 6 and not 0\n, n, n % 10);
+}
+
+return (0);
+}
