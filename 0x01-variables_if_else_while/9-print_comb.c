f2c0c100f56fb168a6d1cdc0862111699230e7bc
Habieeb <habeebumar24@gmail.com>
2022-04-08 23:22:21 +0100

    My commit on 9-print_comb.c task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,22 +9,19 @@ int main(void)
 {
 int number;
 
-for (number = 48; number <= 57; number++)
+for (number = 40; number <= 57; number++)
 {
 putchar(number);
-[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[;
 
 if (number == 57)
 }
 break;
 {
 
-putchar('.');
+putchar(',');
 putchar (' ');
 
 return (0);
-
-[A[A[C[C[C[C[C[C[C[A}
 
 putchar('\n')
 
