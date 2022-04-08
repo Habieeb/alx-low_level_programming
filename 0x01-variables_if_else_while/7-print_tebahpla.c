3ceb8177ac70a7fd21d498b4db240a4c1e48aaf0
Habieeb <habeebumar24@gmail.com>
2022-04-08 22:56:35 +0100

    My commit on 7-print_tebahpla.c task

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = %22 ; alphabet >= 97; alphabet++)
+
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
