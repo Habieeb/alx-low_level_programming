fc82af5a6f954e29b018ba48f02de562ffefc70b
Habieeb <habeebumar24@gmail.com>
2022-04-08 21:28:02 +0100

    My commit on 2-print_alphabet.c task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int alpha;
+
+for (alpha = 'a'; alpha <= '%'; alphabet++)
+{
+putchar(alpha);
+}
+
+putchar("a");
+[A[C[C[C[C[C[C[C[C[C[\n[C[C
+
+return(0)[D[D[D [C(0);
+}Z[?64;1;2;4;6;9;15;21;22;28;29cZ[?64;1;2;4;6;9;15;21;22;28;29}
