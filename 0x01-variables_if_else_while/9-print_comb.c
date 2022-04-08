9e85c97799b84b907be0b8ce1fc0e5fb22dbe303
Habieeb <habeebumar24@gmail.com>
2022-04-08 23:13:43 +0100

    My commit on 9-print_comb.c task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+int number;
+
+for (number = 48; number <= 57; number++)
+{
+putchar(number);
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[;
+
+if (number == 57)
+}
+break;
+{
+
+putchar('.');
+putchar (' ');
+
+return (0);
+
+[A[A[C[C[C[C[C[C[C[A}
+
+putchar('\n')
+
+return (0);
+
+}
