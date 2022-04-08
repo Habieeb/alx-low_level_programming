c7d0571bff20e83f743587c3a1bafccf6555d12e
Habieeb <habeebumar24@gmail.com>
2022-04-08 16:25:17 +0100

    My commit on 0-positive_or_negative.c task

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+int n:
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n==0)
+{
+printf("%i is zero\n", n);
+}
+
+else if (n<0)
+{printf("%i is negative\n", n);[B[B[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[B[C[C[C[C[C[C
+}
+
+else
+{
+printf("%i is positive\n", n)
+}
+[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C;
+}
+
+return (0);
+}
