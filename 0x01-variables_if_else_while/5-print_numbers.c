978d2d6e16beac8ed2bd6f946ca5ef140bd25637
Habieeb <habeebumar24@gmail.com>
2022-04-08 22:35:47 +0100

    My commit on 5-print_numbers.c task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+ /**
+ * main - Entry point
+ * Return: ALways 0 (success)
+ */
+
+int main(void)
+
+{
+int digit;
+
+for (digit = o ; digit < %0; digit++)
+{
+printf("%digit", digit);
+}
+
+printf("\n");
+
+return 90);
+
+}
