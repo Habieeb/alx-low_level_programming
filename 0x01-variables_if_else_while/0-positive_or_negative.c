02fa1396f88e5084250b0ccb06bcedcaf679ef16
Habieeb <habeebumar24@gmail.com>
2022-04-08 17:02:14 +0100

    My commit on 0-positive_or_negative.c task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,14 +20,13 @@ printf("%i is zero\n", n);
 }
 
 else if (n<0)
-{printf("%i is negative\n", n);[B[B[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[B[C[C[C[C[C[C
+{
+printf("%i is negative\n", n);
 }
 
 else
 {
-printf("%i is positive\n", n)
-}
-[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C;
+printf("%i is positive\n", n);
 }
 
 return (0);
