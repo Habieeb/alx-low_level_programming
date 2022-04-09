c982903c9d7b0bf953ddf4e5be8171aefb35d481
Habieeb <habeebumar24@gmail.com>
2022-04-09 20:26:22 +0100

    My commit on 8-print_base16.c task

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,21 +9,15 @@ int main(void)
 {
 char base_16;
 
-for (base_16 = 97n; base_16 <= 102; base_16++)
+for (base_16 = 48; base_16 <= 57; base_16++)
 {
-
-for (base_16 = [A[A[A[B[B[B[C[C[D[A[A[5[8[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C 48; base_16 <= 57; base_16++)
-[C
-putchar(base_16)
-
-[A[C[C[C[C[C[C[C[C[C[[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D
-[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[C[C;
+putchar(base_16);
 }
 
-for (base_16 =97 ; base_16 <= 102; base_16++)
+for (base_16 = 97 ; base_16 <= 102; base_16++)
 {
-putchar(base_16)
-[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C;
+putchar(base_16);
+
 }
 
 putchar ('\n")
