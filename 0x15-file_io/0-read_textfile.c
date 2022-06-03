d791ab5e5a3e225df510ffd001cc4555cb002c41
Habieeb <habeebumar24@gmail.com>
2022-06-02 17:38:23 -0700

    0-read_textfile.c file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,18 +13,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int i, y;
 	char *buf;
 	if (!filename)
-		return(0);
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
-		return(0);
+		return (0);
 	buf = malloc(sizeof(char) * letters);
 	if (!buf)
-		return(0);
+		return (0);
 	i = read(fd, buf, letters);
 	if (i < 0)
 	{
 		free(buf);
-		return(0);
+		return (0);
 	}
 	buf[i] = '\0';
 	close(fd);
@@ -32,7 +32,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (y < 0)
 	{
 		free(buf);
-		return(0);
+		return (0);
 	}
 	free(buf);
 	return (y);
